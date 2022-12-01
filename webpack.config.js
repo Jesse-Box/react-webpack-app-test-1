ebc36670821de1682a57acff322817be77b15185
Jesse Box <me@jessebox.net>
2022-12-01 16:49:46 +0100

    [Broken] Webpack while installing PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html',
-});
+const path = require('path');
+
+const htmlWebPackPlugin = require('html-webpack-plugin');
+const { cleanWebpackPlugin } = require('clean-webpack-plugin');
+const { postcssPresetEnv } = require('postcss-preset-env');
+
 module.exports = {
   mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
   module: {
     rules: [
+      // JavaScript
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -14,11 +23,30 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      // CSS
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
       },
     ],
   },
-  plugins: [htmlPlugin],
+  plugins: [
+    new htmlWebPackPlugin({
+      title: 'react-webpack-app',
+      template: './src/template.html',
+      filename: './index.html',
+    }),
+    new cleanWebpackPlugin(),
+    new postcssPresetEnv(),
+  ],
 };
