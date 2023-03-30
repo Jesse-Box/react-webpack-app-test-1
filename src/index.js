cf029c80a807e5ec062d1650e0e1312b78f03664
Jesse Box <me@jessebox.net>
2023-03-30 12:44:52 +0200

    Added: Sentry instrumentation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://36d476f30112490886ebf7af8a69ce80@o4504792325292032.ingest.sentry.io/4504792408850432",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 function App() {
   return (
     <div>
       <h1>This is my React app!</h1>
-      <button>Send Error</button>
+      <button onClick={() => methodDoesNotExist()}>Send Error</button>
     </div>
   );
 }
